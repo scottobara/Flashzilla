5b9618de4fbe860e8100d48507a047f4c374463c
Scott Obara <scott@rubbscreations.com>
2021-03-13 16:03:01 +1100

    Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,23 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped!")
+        Text("Hello, World!")
+            .onReceive(timer) { time in
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
                 }
 
-            Circle()
-                .fill(Color.red)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Circle tapped!")
-                }
-                .allowsHitTesting(false)
-        }
+                self.counter += 1
+            }
     }
 }
 
